95342cf7e2e66032565d6456eab894a88ef8c816
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-08 20:51:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,13 +30,13 @@ class Song
   end
 
   def self.genre_count
-    genre_count = {}
+    genre_count = {"genre"}
     @@genres.each {|genre| genre_count[genre] += 1}
     genre_count
   end
 
   def self.artist_count
-    artist_count = {}
+    artist_count = {"artist"}
     @@artists.each {|artist| artist_count[artist] += 1}
     artist_count
   end
