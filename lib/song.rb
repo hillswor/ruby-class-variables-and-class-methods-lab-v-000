ed85723d7e4035c3bbf691b4c2b7f7f7f925e575
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-08 20:06:34 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+require "pry"
+
+class Song
+
+  attr_accessor :name, :song_artist, :song_genre
+
+  @@count = 0
+
+  def initialize(song_name, song_artist, song_genre)
+    @@count += 1
+  end
+
+  def name
+    puts @name
+  end
+
+  def self.count
+    @@count
+  end
+
+end
+binding.pry 
